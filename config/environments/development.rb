366555bc02f2ccc1408b79175583d969e800884a
Ravi Soni <ravi.soni45452@gmail.com>
2024-12-08 21:08:57 +0530

    some Inhancements

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -88,6 +88,6 @@ config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # config.action_cable.disable_request_forgery_protection = true
 
   #mailcatcher or showing outgoing mails
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
 
 end
